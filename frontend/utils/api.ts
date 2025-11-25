88525e71c18c497c62d4eb31326d8e2f94423896
nuhaaaaan <102981510+nuhaaaaan@users.noreply.github.com>
2025-11-24 22:44:13 -0500

    Format API_URL declaration for better readability

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import Constants from 'expo-constants';
 
-const API_URL = Constants.expoConfig?.extra?.EXPO_PUBLIC_BACKEND_URL || process.env.EXPO_PUBLIC_BACKEND_URL;
+const API_URL =
+  Constants.expoConfig?.extra?.EXPO_PUBLIC_BACKEND_URL ||
+  process.env.EXPO_PUBLIC_BACKEND_URL;
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
